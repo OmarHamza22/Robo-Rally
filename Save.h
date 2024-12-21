d6de0c7df4eefde63a4f34b2272c1edc343d0efc
AliIbrahim174 <141499297+AliIbrahim174@users.noreply.github.com>
2024-12-22 01:46:36 +0200

    Merge branch 'main' of https://github.com/OmarHamza22/Final-Phase

--- Save.h
+++ Save.h
@@ -4,14 +4,18 @@ class Save :
     public Action
 {
 private:
-
-
+    string filename;
 
 public:
+    
+    Save(ApplicationManager* pApp) :Action(pApp)
+    {
+    };
 
     void ReadActionParameters() override;
 
     void Execute() override;
 
+    virtual ~Save();
 };
 
